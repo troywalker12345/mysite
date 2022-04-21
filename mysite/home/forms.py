e659acf87c9d6db7ef11fd70b0b1f9b70e56e614
Troy Walker <troywalker12345@gmail.com>
2022-04-21 14:17:46 +0100

    index changes

--- mysite/home/forms.py
+++ mysite/home/forms.py
@@ -10,7 +10,7 @@ class InputForm(forms.Form):
 	advertisement_as_monetization = forms.IntegerField(label='On a scale from 1-10 (1 being bad), what is your current view of advertisement as a monetization option?', required=True, min_value=1, max_value=10)
 	mining_intensity = forms.IntegerField(widget=forms.HiddenInput())
 	performance_hits = forms.IntegerField(label='On a scale of 1-10 (1 being not affected), how much did you notice a performance drop on your device?', required=True, min_value=1, max_value=10)
-	experience = forms.IntegerField(label='On a scale of 1-10 (1 being not affected), how much does the mining in the browser affect your browsing experince?', required=True, min_value=1, max_value=10)
+	experience = forms.IntegerField(label='On a scale of 1-10 (1 being not affected), how much does the mining in the browser affect your browsing experience?', required=True, min_value=1, max_value=10)
 	consider_solution = forms.IntegerField(label='On a scale of 1-10 (1 being a bad alternative), from your experience on this page, and the information provided above, what is your view towards cryptocurrency mining in the browser as an alternative solution to advertisement?', required=True, min_value=1, max_value=10)
 	sex = forms.CharField(label='What is your sex?', widget=forms.Select(choices=CHOICES))
 	age = forms.IntegerField(label='What is your age?', required=True, min_value=0, max_value=125)
