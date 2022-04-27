8e5169372139e4e2ffefdf0489f4ffc29e7f6d5a
Troy Walker <troywalker12345@gmail.com>
2022-04-27 16:25:56 +0100

    Multiple last minute improvements to JS and HTML

--- mysite/home/forms.py
+++ mysite/home/forms.py
@@ -7,7 +7,7 @@ class InputForm(forms.Form):
 	heard_of_cryptocurrency = forms.BooleanField(label='Have you ever heard of cryptocurrency? (Tick for yes)', required=False)
 	purchased_cryptocurrency = forms.BooleanField(label='Have you ever purchased cryptocurrency? (Tick for yes)', required=False)
 	consider_valid = forms.BooleanField(label='Do you consider cryptocurrency a valid form of currency? (Tick for yes)', required=False)
-	advertisement_as_monetization = forms.IntegerField(label='On a scale from 1-10 (1 being bad), what is your current view of advertisement as a monetisation option?', required=True, min_value=1, max_value=10)
+	advertisement_as_monetization = forms.IntegerField(label='On a scale from 1-10 (1 being bad), what is your current view of advertising as a monetisation option?', required=True, min_value=1, max_value=10)
 	mining_intensity = forms.IntegerField(widget=forms.HiddenInput())
 	performance_hits = forms.IntegerField(label='On a scale of 1-10 (1 being not affected), how much did you notice a performance drop on your device?', required=True, min_value=1, max_value=10)
 	experience = forms.IntegerField(label='On a scale of 1-10 (1 being not affected), how much does the mining in the browser affect your browsing experience?', required=True, min_value=1, max_value=10)
